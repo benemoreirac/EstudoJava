5874c168a246653ea76b163e041fe6fbd5294a00
Ebenézer Moreira <82347599+benemoreirac@users.noreply.github.com>
2022-01-19 18:37:48 -0300

    Add files via upload

---
+++ Aula2.java
@@ -0,0 +1,17 @@
+public class Aula2{
+    public static void main(String [] args){
+        int n1 = 0, n2 = 0, n3 = 0;
+        int res = n1+n2+n3;
+        System.out.println("A Soma de res é: " + res);
+        n1 = 10;
+        n2 = 20;
+        n3 = 30;
+        int soma2 = n1+n2+n3;
+        System.out.println("A Soma de res é: " + soma2);
+        int num = 50;
+        String nome = "Bene";
+        System.out.println("CursoCFB");
+        System.out.println(num);
+        System.out.println(nome);
+    }
+}
