9c3ba532d0843954206d188cd8f8519e539d5d8f
Ebenézer Moreira <82347599+benemoreirac@users.noreply.github.com>
2022-02-01 11:56:44 -0300

    Add files via upload

---
+++ JavaScript/CursoGLemos/comments.js
@@ -0,0 +1,21 @@
+//COmentario em linha
+var x = 4;
+var y = x+2;
+
+console.log("Y é a soma totatl de x + 2", y)
+
+console.log('Oi!')// coment resto de linha
+
+/*
+    Comentario
+    de varias
+    linhas;
+*/
+const cachorro = {
+    nome: 'Diamond baby',
+    raca: 'Chihuaha',
+    cor: 'marro/preto'
+}
+console.log(cachorro);
+
+console.log('Fim de comentarios e MultilieComents');
---
+++ JavaScript/CursoGLemos/olaMundo.js
@@ -0,0 +1,5 @@
+const lugar = "Mundo";
+const saudacao = "Olá"
+
+console.log("Helo World!");
+console.log(`${saudacao}, ${lugar}`);
