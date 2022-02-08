28191c170ccd45333aec2ed6ce416780e701d237
Ebenézer Moreira <82347599+benemoreirac@users.noreply.github.com>
2022-02-08 11:58:20 -0300

    Add files via upload

---
+++ Java/CursoCFB/Aula10.java
@@ -0,0 +1,12 @@
+
+public class Aula10{
+    public static void main(String [] args){
+
+        int num =0;
+        Jogador player1 = new Jogador(1);
+        Jogador player2 = new Jogador(2);
+        Jogador player3 = new Jogador(3);
+
+        player2.tocaBola();
+    }
+}
---
+++ Java/CursoCFB/Jogador.java
@@ -0,0 +1,10 @@
+public class Jogador {
+    Jogador (int m){
+        System.out.printf("%n Jogador numero %d criado %n", m);
+    }
+
+    public void tocaBola(){
+        System.out.println("TocaBola");
+    }
+
+}
