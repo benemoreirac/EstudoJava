117c401df75b9585ae15258ae709e374bfcc786a
Ebenézer Moreira <82347599+benemoreirac@users.noreply.github.com>
2022-06-16 09:41:40 -0300

    Add files via upload

---
+++ AluraCurso/sintaxe-variaveis-e-fluxo/src/TestaCondicional2.java
@@ -0,0 +1,17 @@
+
+public class TestaCondicional2 {
+	public static void main(String[] args) {
+		System.out.println("Testando condicionais");
+		int idade = 16;
+		int quantidadeDePessoas = 1;
+		
+		
+		if (idade >= 18 && quantidadeDePessoas >=2) {
+			System.out.println("Seja bem vindo");
+		} else {
+			System.out.println("Infelizmente voce nao pode entrar");
+					
+		}
+	}
+
+}
