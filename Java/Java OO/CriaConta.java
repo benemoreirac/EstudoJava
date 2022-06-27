cc05778043809d3e9bc9b4648771f88642d596f0
Ebenézer Moreira <82347599+benemoreirac@users.noreply.github.com>
2022-06-27 06:41:22 -0300

    Add files via upload

---
+++ Java/Java OO/CriaConta.java
@@ -0,0 +1,22 @@
+public class CriaConta {
+	
+	public static void main(String[] args) {
+		
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 200;
+		System.out.println(primeiraConta.saldo);
+		
+		primeiraConta.saldo += 100;
+		System.out.println(primeiraConta.saldo);
+		
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 50;
+		System.out.println(segundaConta.saldo);
+		
+		if(primeiraConta == segundaConta) {
+			System.out.println("São a mesma conta!");
+		}else {
+			System.out.println("Contas diferentes!");
+		}
+	}
+}
